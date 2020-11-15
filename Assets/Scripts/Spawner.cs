2be5ea4dff8530a939ddd8a1f23a7faa6bf55202
Abhishek <abhishekm942@gmail.com>
2020-11-15 17:00:11 +0530

    basic scene setup

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] cubes;
+    public Transform[] spawnPoints;
+    public float beat = 120/130;
+    float timer;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (timer > beat)
+        {
+            GameObject cube = Instantiate(cubes[UnityEngine.Random.Range(0, 2)], spawnPoints[UnityEngine.Random.Range(0, 4)]);
+            cube.transform.localPosition = Vector3.zero;
+            cube.transform.Rotate(transform.forward, 90 * UnityEngine.Random.Range(0, 4));
+            timer -= beat;
+        }
+
+        timer += Time.deltaTime;
+    }
+}
